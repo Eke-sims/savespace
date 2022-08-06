60b92ab2fb3eef88566dc65bbdfca5acbee44093
Eke-sims <Eketimileyin@gmail.com>
2022-08-06 16:44:17 +0100

    made the 'more' section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import './footer.css'
-import logo from '../../assets/logo.png'
+import logo from '../../assets/logo white.png'
 import facebook from '../../assets/facebookIcon.png'
 import twitter from '../../assets/twitterIcon.png'
 import instagram from '../../assets/InstagramIcon.png'
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className='section__padding ss__footer' id='footer'>
       <div className="ss__footer_links-container">
-        <div className='ss__footer_link-section'>
+        <div className='ss__footer_link-section logo-section'>
           <img src={logo} alt="logo" className="ss__footer_logo" />
           <p>SaveSpace is the world largest and most secure digital savings service</p>
           <div className="ss__footer_social-link-container">
@@ -56,7 +56,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="ss__footer_copy">
-        <p className="ss__footer_copy-text">made with love</p>
+        <p className="ss__footer_copy-text">© 2022 SaveSpace - Made with</p>
       </div>
     </footer>
   )
--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -1,8 +1,22 @@
 import './more.css'
+import girl from '../../assets/girlImg.png'
 
 const More = () => {
   return (
-    <div>More</div>
+    <section className="section__padding ss__more">
+      <div className="ss__more_image-container">
+        <img src={girl} alt="girl" />
+      </div>
+      <div className="ss__more_content">
+        <h1 className="ss__more_content_title">What else can you do with SaveSpace?</h1>
+        <ul className="ss__more_content_list">
+          <li>Are you scared of participating in ajo, esusu or adashe? SaveSpace allows you participate in thrift worry free.</li>
+          <li>Accept payments from all banks. Several features on the app ensure maximum guarantee and transparency.</li>
+          <li>SaveSpace allows you to invest your money in different industries, with mouth watering ROI.</li>
+        </ul>
+        <button className='learn-more'>Learn More</button>
+      </div>
+    </section>
   )
 }
 
